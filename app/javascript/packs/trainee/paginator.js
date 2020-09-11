5f6a9fad48fcf4daaf5dd8770caad700037e6236
anhnt-1044 <nguyen.trung.anh@sun-asterisk.com>
2020-09-11 13:18:40 +0700

    fix kaminari windown scroll

--- app/javascript/packs/trainee/paginator.js
+++ app/javascript/packs/trainee/paginator.js
@@ -1,11 +1,12 @@
-export function scrollPaginator(){
+export function scrollPaginator() {
   if ($('#subject-paginator .pagination').length && $('#list-subjects').length) {
-    $(window).on('scroll', function(){
-      let more_subjects_url = $('#subject-paginator .pagination a[rel]').attr('href');
-      if(more_subjects_url) {
-        more_subjects_url = more_subjects_url;
+    $(window).on('scroll', function() {
+      let next_item = $('#subject-paginator .pagination .active').next();
+      let more_subjects_url;
+      if (next_item.length == 1) {
+        more_subjects_url = $(next_item).children().first().attr("href");
       } else {
-        more_subjects_url= $('#subject-paginator .pagination .next a').attr('href');
+        more_subjects_url = $('#subject-paginator .pagination .active a').first().attr('href');
       }
       if (more_subjects_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
         $('#subject-paginator .pagination').html('<img src="/assets/widget-loader-lg-en.gif" height="40" />');
